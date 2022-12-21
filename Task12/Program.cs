3f2003ca53fbf38de50acbb57bd9ab8a33aa99a1
Einzel <einzel0102@mail.ru>
2022-12-21 21:56:22 +0800

    начал 12 задачу

---
+++ Task12/Program.cs
@@ -0,0 +1,21 @@
+﻿int Middle(int argument)
+{
+     if( argument < 100)
+     {
+        Console.WriteLine("Третьей цифры нет");
+     }
+     if (argument < 1000)
+     {
+        int newargument = argument % 10;
+        return newargument;
+     }
+     if (argument < 10000)
+     {
+        int newargument = (argument /10) % 10;
+        return newargument;
+     }
+}
+
+
+
+
