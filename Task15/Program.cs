662e915e47086254906436e61d49bca7e97643d4
Einzel <einzel0102@mail.ru>
2022-12-22 20:00:08 +0800

    решил задачу 15.1

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,8 +1,8 @@
 ﻿int Daysoftheweek(int numberDay)
 {
-    if (numberDay < 6) Console.WriteLine("Иди на работу");
-    if (numberDay == 6) Console.WriteLine("Лежи дома на диванчике, выходной же");
-    if (numberDay == 7) Console.WriteLine("Лежи дома на диванчике, выходной же");
+    if (numberDay < 6) Console.WriteLine("Рабочий день");
+    if (numberDay == 6) Console.WriteLine("Выходной");
+    if (numberDay == 7) Console.WriteLine("Выходной");
     if (numberDay > 7) Console.WriteLine("Такого дня недели нет");
     return numberDay;
 }
