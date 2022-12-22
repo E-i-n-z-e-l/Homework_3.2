f165718ca2471d40ed28828745fa5b685e4e463e
Einzel <einzel0102@mail.ru>
2022-12-22 19:58:48 +0800

    решил задачу 15

---
+++ Task15/Program.cs
@@ -0,0 +1,12 @@
+﻿int Daysoftheweek(int numberDay)
+{
+    if (numberDay < 6) Console.WriteLine("Иди на работу");
+    if (numberDay == 6) Console.WriteLine("Лежи дома на диванчике, выходной же");
+    if (numberDay == 7) Console.WriteLine("Лежи дома на диванчике, выходной же");
+    if (numberDay > 7) Console.WriteLine("Такого дня недели нет");
+    return numberDay;
+}
+
+Console.WriteLine("Введите число обозначающие день недели");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Daysoftheweek(number));
