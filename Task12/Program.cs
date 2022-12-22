d3df524a1bc0e4b52a268869ce5d201681619f70
Einzel <einzel0102@mail.ru>
2022-12-22 19:14:55 +0800

    решил задачу 13

--- Task12/Program.cs
+++ Task12/Program.cs
@@ -1,20 +1,39 @@
-﻿int Middle(int argument)
+﻿int MiddleDigit(int digit)
 {
-     if( argument < 100)
-     {
-        Console.WriteLine("Третьей цифры нет");
-     }
-     if (argument < 1000)
-     {
-        int newargument = argument % 10;
-        return newargument;
-     }
-     if (argument < 10000)
-     {
-        int newargument = (argument /10) % 10;
-        return newargument;
-     }
+    while (digit > 999)
+    {
+        digit = (digit / 10);
+    }
+    return digit % 10;
 }
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры тю-тю");
+}
+else
+{
+    Console.WriteLine(MiddleDigit(number));
+}
+
+
+
+
+
+//{
+//    Console.WriteLine(Middle(number));
+//}
+
+//int res = 98765;
+//while (res > 999)
+//{
+//    res = res / 10;
+//}
+//Console.Write(res);
+
 
 
 
